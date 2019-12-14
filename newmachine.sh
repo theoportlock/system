3fafdaa1efaf310e4b518873e396e05a6a3f0bfa
theoportlock <zn.tportlock@gmail.com>
2019-12-14 18:54:36 +0000

    fixing the savecurrc

--- newmachine.sh
+++ newmachine.sh
@@ -3,4 +3,5 @@ hostnam=$(hostname)
 if [[ ! -e ~/system/computers/$hostnam ]]; then
 	mkdir ~/system/computers/$hostnam
 	screenfetch -Nn > ~/system/computers/$hostnam/stats
+	echo "name=$hostnam" > ~.computer
 fi
