8881bfea0c86c6ff9e4978c130cd48a025ac5024
theoportlock <zn.tportlock@gmail.com>
2020-02-04 18:27:15 +0000

    works!

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,21 +8,25 @@ setup_grive=$(sed -n -e 's/^setup_grive=//p' ~/.computer)
 fix_history_settings=$(sed -n -e 's/^fix_history_settings=//p' ~/.computer)
 stats=$(sed -n -e 's/^stats=//p' ~/.computer)
 
-if [[ $copy_wifi_profiles == "y"]]; then
+if [ $copy_wifi_profiles == "y" ]
+then
 	sudo cp -r ~/system/wifiprofiles/* /etc/wpa_supplicant/; 
 fi
 
-if [[ $install_pacman_programs == "y"]]; then
+if [ $install_pacman_programs == "y" ]
+then
 	pacman --needed -S - < ~/system/computers/$hostnam/programlist
 fi
 
-if [[ $install_git_repositories == "y"]]; then
+if [ $install_git_repositories == "y" ]
+then
 	cd &&
 	git clone https://github.com/theoportlock/thoughts.git ;
 	git clone https://github.com/theoportlock/tis.git 
 fi
 
-if [[ $setup_grive == "y"]]; then
+if [ $setup_grive == "y" ]
+then
 	cd ~ &&
 	git clone https://aur.archlinux.org/yay.git &&
 	cd yay &&
@@ -35,11 +39,13 @@ if [[ $setup_grive == "y"]]; then
 	grive -a
 fi
 
-if [[ $fix_history_settings == "y"]]; then
+if [ $fix_history_settings == "y" ]
+then
 	shopt -s histappend &&
 	shopt -s cmdhist
 fi
 
-if [[ $stats == "y"]]; then
+if [ $stats == "y" ]
+then
 	screenfetch -Nn > ~/system/computers/$hostnam/stats
 fi
