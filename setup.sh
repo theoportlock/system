2642e439da3c4585843cb93f38925d4cc0c423ed
theoportlock <zn.tportlock@gmail.com>
2020-03-01 19:20:38 +0000

    fixing setup and conversion scripts

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 hostnam=$(hostname)
 
+./scripts/makecomputerfile &&
+
 copy_wifi_profiles=$(sed -n -e 's/^copy_wifi_profiles=//p' ~/.computer)
 install_pacman_programs=$(sed -n -e 's/^install_pacman_programs=//p' ~/.computer)
 install_git_repositories=$(sed -n -e 's/^install_git_repositories=//p' ~/.computer)
-setup_grive=$(sed -n -e 's/^setup_grive=//p' ~/.computer)
 fix_history_settings=$(sed -n -e 's/^fix_history_settings=//p' ~/.computer)
 stats=$(sed -n -e 's/^stats=//p' ~/.computer)
 
@@ -23,20 +24,6 @@ then
 	cd &&
 	git clone https://github.com/theoportlock/thoughts.git ;
 	git clone https://github.com/theoportlock/tis.git 
-fi
-
-if [ $setup_grive == "y" ]
-then
-	cd ~ &&
-	git clone https://aur.archlinux.org/yay.git &&
-	cd yay &&
-	makepkg -si &&
-	yay -S grive &&
-	mkdir ~/tdrive &&
-	cd ~/tdrive &&
-	grive -a &&
-	mkdir ~/gdrive &&
-	grive -a
 fi
 
 if [ $fix_history_settings == "y" ]
