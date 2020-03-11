f7f8821f953f494c9fb3eaddda458c3b3ec86f60
theoportlock <zn.tportlock@gmail.com>
2020-03-11 23:05:14 +0000

    looking sharp, removed the make computer file and made setup more straight forward

--- setup.sh
+++ setup.sh
@@ -2,25 +2,43 @@
 hostnam=$(hostname)
 
 # install programs and copy rc files from another computer
-./scripts/makecomputerfile &&
-
-copy_wifi_profiles=$(sed -n -e 's/^copy_wifi_profiles=//p' ~/.computer)
-copy_rcfiles_from_computer=$(sed -n -e 's/^copy_rcfiles_from_computer=//p' ~/.computer)
-install_programs=$(sed -n -e 's/^install_programs=//p' ~/.computer)
-install_git_repositories=$(sed -n -e 's/^install_git_repositories=//p' ~/.computer)
-fix_history_settings=$(sed -n -e 's/^fix_history_settings=//p' ~/.computer)
-stats=$(sed -n -e 's/^stats=//p' ~/.computer)
-
-if [ $copy_wifi_profiles == "y" ]
-then
-	sudo cp -r ~/system/wifiprofiles/* /etc/wpa_supplicant/; 
+hostnam=$(hostname)
+wificard=$(ip link | grep -m 1 -o "w\w\+[^:]")
+if [[ ! -e ~/.computer ]]; then
+	echo "name=$hostnam" >> ~/.computer
+	echo "wifi_card=$wificard" >> ~/.computer
+	echo "copy_computer_profile_name=" >> ~/.computer
+	echo "package_manager_install_command=" >> ~/.computer
+	echo "copy_rcfiles=n" >> ~/.computer
+	echo "install_bash_programs=n" >> ~/.computer
+	echo "install_pip_programs=n" >> ~/.computer
+	echo "install_git_repositories=n" >> ~/.computer
+	echo "setup_rclone=n" >> ~/.computer
+	vi ~/.computer
+	exit
 fi
 
-if [ $install_programs== "y" ]
+copy_computer_profile_name=$(sed -n -e 's/^copy_computer_profile_name=//p' ~/.computer)
+package_manager_install_command=$(sed -n -e 's/^package_manage_install_commandr=//p' ~/.computer)
+copy_rcfiles=$(sed -n -e 's/^copy_rcfiles=//p' ~/.computer)
+install_bash_programs=$(sed -n -e 's/^install_bash_programs=//p' ~/.computer)
+install_pip_programs=$(sed -n -e 's/^install_pip_programs=//p' ~/.computer)
+install_git_repositories=$(sed -n -e 's/^install_git_repositories=//p' ~/.computer)
+setup_rclone=$(sed -n -e 's/^setup_rclone=//p' ~/.computer)
+
+if [ $copy_rcfiles == "y" ]
 then
-	sudo apt install $(cat ~/system/computers/$hostnam/programlist)
-	pacman --needed -S - < ~/system/computers/$hostnam/programlist
-	sudo pip install -r ~/system/computers/$hostnam/pythonlist
+	cp -r ~/system/computers/$(copy_computer_profile_name)/rcfiles/* ~
+fi
+
+if [ $install_bash_programs == "y" ]
+then
+	sudo $(package_manage_install_command) $(cat ~/system/computers/$(copy_computer_profile_name)/programlist)
+fi
+
+if [ $install_pip_programs == "y" ]
+then
+	sudo pip3 install -r ~/system/computers/$(copy_computer_profile_name)/pythonlist
 fi
 
 if [ $install_git_repositories == "y" ]
@@ -35,13 +53,9 @@ then
 	git clone https://github.com/theoportlock/todo.git
 fi
 
-if [ $fix_history_settings == "y" ]
-then
-	shopt -s histappend &&
-	shopt -s cmdhist
-fi
+# history settings
+shopt -s histappend &&
+shopt -s cmdhist
 
-if [ $stats == "y" ]
-then
-	screenfetch -Nn > ~/system/computers/$hostnam/stats
-fi
+# stats
+screenfetch -Nn > ~/system/computers/$hostnam/stats
