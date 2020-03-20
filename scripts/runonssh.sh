979662415d1754cc127a719ed2f91457af94b24b
theoportlock <zn.tportlock@gmail.com>
2020-03-20 13:11:42 +0000

    just cleaning up the scripts

--- scripts/runonssh.sh
+++ scripts/runonssh.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -xv
-
 remote_dir=/home/nmrbox/tportlock/
 remote_addr='tportlock@tportlock.nmrbox.org'
 script=$1
@@ -7,8 +6,3 @@ input=$2
 scp -r $input $remote_addr:$remote_dir &&
 scp -r $script $remote_addr:$remote_dir &&
 ssh $remote_addr "${remote_dir}$script $input"
-
-#if [ $? == 0 ]; then
-#	scp -r tportlock@tportlock.nmrbox.org:"/home/nmrbox/tportlock/aria/${nam}/run${run}/structures/it8 /home/nmrbox/tportlock/aria/${nam}/run${run}/structures/refine /home/nmrbox/tportlock/aria/${nam}/run${run}analysis" .
-#	ssh tportlock@tportlock.nmrbox.org "rm -r /home/nmrbox/tportlock/aria/${nam}/${run}"
-#fi
