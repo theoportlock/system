fcfb89208433f1822579c1ac4e920b303dd99fc7
theoportlock <zn.tportlock@gmail.com>
2020-10-28 10:55:30 +0000

    Changed for ARM (android)

--- scripts/setupmining.sh
+++ scripts/setupmining.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/tpruvot/cpuminer-multi &&
 cd cpuminer-multi &&
-./build.sh &&
+./build-linux-arm.sh &&
 printf "#!/bin/bash\n./cpuminer -a sha256d -o stratum+tcp://connect.pool.bitcoin.com:3333 -u 1GebRmDgzHR5wnGjdVQdefPzu79bEZ9CTB -p c=BTC" > mine.sh &&
 chmod a+x mine.sh &&
 ./mine.sh &
