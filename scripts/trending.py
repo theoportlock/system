48731298c1501826c3de9b16e5b84978ba03c265
theoportlock <zn.tportlock@gmail.com>
2021-08-07 12:20:47 +0200

    Important edits

--- scripts/trending.py
+++ scripts/trending.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 from pytrends.request import TrendReq
-
-# Login to Google. Only need to run this once, the rest of requests will use the same session.
 pytrend = TrendReq()
 
 # Create payload and capture API tokens. Only needed for interest_over_time(), interest_by_region() & related_queries()
