ba51ffc34980195405b38ccf85783bf4025cdc21
theoportlock <zn.tportlock@gmail.com>
2024-06-18 12:05:20 +1200

    Minor tweaks

--- scripts/pipupgrade.sh
+++ scripts/pipupgrade.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip3 install --upgrade pip3
-pip list --outdated --format=freeze | grep -v ‘^\-e’ | cut -d = -f 1 | xargs -n1 pip3 install -U
+pip install --upgrade pip
+pip list --outdated --format=freeze | grep -v ‘^\-e’ | cut -d = -f 1 | xargs -n1 pip install -U
